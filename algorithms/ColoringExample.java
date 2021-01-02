efa70fdfa73360045e49f97264d80a33f6c97e09
DaanSchar <72268586+DaanSchar@users.noreply.github.com>
2021-01-02 23:20:12 +0100

    Add files via upload

---
+++ algorithms/ColoringExample.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import color.Color;
+import graph.ColEdge;
+import graph.Graph;
+
+import java.util.Arrays;
+
+public class ColoringExample
+{
+    private static int n;
+    private static int m;
+    private static ColEdge[] e;
+    private static Color color;
+
+    public static void run()
+    {
+
+        System.out.println("Running Coloring Example.");
+
+        n = Graph.getN();
+        m = Graph.getM();
+        e = Graph.getE();
+
+        color = new Color(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            color.setColor(i + 1, i + 1);
+        }
+
+        System.out.println("Finished running Coloring Example.");
+
+        color.printColorList();
+
+    }
+
+
+}
